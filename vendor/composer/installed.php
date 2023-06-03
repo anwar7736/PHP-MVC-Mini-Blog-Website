222995973fcd8014fecea6fa265d87e7a652b31d
anwar7736 <anwarhossain7736@gmail.com>
2023-06-03 20:46:15 +0600

    Final Changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ac/php-mvc-blog',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e042893cb419e0c12b57505e4b580a1d030ed1f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ac/php-mvc-blog' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e042893cb419e0c12b57505e4b580a1d030ed1f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
